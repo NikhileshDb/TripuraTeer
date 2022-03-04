02bf268e073dc903e940b726c21113d3d9f6d145
Nikhilesh Debbarma <nikhilesh.debbarma062@gmail.com>
2022-03-05 01:59:47 +0530

    added the postgresql database

--- teer/settings.py
+++ teer/settings.py
@@ -79,12 +79,24 @@ WSGI_APPLICATION = 'teer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'tripura_teer_db',
+        'USER': 'tripurateer',
+        'PASSWORD': 'Nikhilesh@143', 
+        'HOST': '194.163.43.110',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
