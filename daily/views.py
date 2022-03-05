b03626f3faca874aaaaf264b4abcffe2bdd93189
Nikhilesh Debbarma <nikhilesh.debbarma062@gmail.com>
2022-03-05 22:14:39 +0530

    fixed mobile responsive

--- daily/views.py
+++ daily/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from .models import DailyResult
-
+from sunday.models import SundayResult
 
 # Create your views here.
 def DailyHomePage(request):
     result = DailyResult.objects.all().order_by('-id')
+    sunday = SundayResult.objects.all().order_by('-id')
     context = {
-        'result': result
+        'result': result,
+        'sunday': sunday,
     }
     return render(request, 'frontend/home_page.html', context)
 
