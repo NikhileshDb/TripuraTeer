a26ecb9ab0f41ec35bc1565a79a9e974ab167c42
Nikhilesh Debbarma <nikhilesh.debbarma062@gmail.com>
2022-03-07 21:26:02 +0530

    klklk

--- daily/urls.py
+++ daily/urls.py
@@ -3,8 +3,8 @@ from .views import DailyHomePage, DailyGamePreviousResult
 
 
 urlpatterns = [
-    path('', DailyHomePage, name="daily_home"),
-    path('daily-game-previous-result/', DailyGamePreviousResult, name="daily_game_previous_result"),
+    path('', DailyHomePage, namespace="daily_home"),
+    path('daily-game-previous-result/', DailyGamePreviousResult, namespace="daily_game_previous_result"),
    
 ]
 
